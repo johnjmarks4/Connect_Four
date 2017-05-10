331fdddd2b0ce39b7a0bcdcbf7b62a05abaf22a5
John Marks <johnjmarks4@gmail.com>
2017-05-10 16:54:53 -0500

    Recently finished with RSpec also finished

--- connect_four.rb
+++ connect_four.rb
@@ -1,17 +1,14 @@
 class Cage
+	attr_accessor :cage
 
 	def initialize
 		@cage = Array.new(6).map { Array.new(7) }
 		@cage.each { |rows| rows.map! { |circles| circles = " " } }
 		@moves = 0
-	end
-	
-	def player_turn
-	  @player_turn
+		@winner = false
 	end
 	
 	def print_cage
-    i = -1
     row1 = @cage[0][0..7]
     row2 = @cage[1][0..7]
     row3 = @cage[2][0..7]
@@ -32,154 +29,92 @@ class Cage
     37.times { print "-" }
   end
 
+  def cage #included for testing purposes
+  	@cage
+  end
+
   def moves
   	@moves
   end
-
-	def cage
-		@cage
-	end
-
-	def check(position, player)
-		player.picked.include?(position)
-	end
 	
 	def prompt_player(player)
 	  @player_turn = player.color
 	  if player.color == "r"
-	    puts "Player red make move"
-	 else
-	  	puts "Player black make move"
-	 end
-	  input = gets.chomp.split(',').map { |e| e.to_i }
+	    puts "Player red move"
+	  else
+	  	puts "Player black move"
+	  end
+	  input = gets.chomp.to_i
     make_move(input, player)
 	end
 
-	def make_move(input, player)
-		row = input[0]
-		column = input[1]
-		if row > 5 || column > 6
-			"Select a value up to 5 for row and 6 for column"
+	def make_move(column, player)
+	  row = find_free(column)
+		if column > 6
+			puts "Select a value between 0 and 6"
+			prompt_player(player)
+		elsif free_spaces?(column) != true
+		  puts "That column is already full"
+		  prompt_player(player)
 		else
 			@moves += 1
 			@cage[row][column] = player.color
 		end
 	end
-
-	def check_rows
-		i = 0
-		discs = []
-		@cage.each do |row|
-			row.each do |circle|
-				discs << circle
-				if circle == discs[-2] && circle != " "
-					i += 1
-					if i == 3
-					  if circle == discs[-1]
-					  	i +=1 
-					  else
-					  	i = 0
-					  end
-					end
-				else
-					i = 0
-				end
-				if i >= 4
-					return true
-				end
-			end
-		end
-		false
+	
+	def find_free(column)
+	  row = @cage.find_index { |row| row[column] != " " }
+	  row != nil ? row - 1 : 5
 	end
-
-	def check_for_vertical_duplicates(c_indices, r_indices)
-		duplicates = c_indices.select { |e| c_indices.count(e) >= 4 }
-		duplicates != [] && r_indices.sort.each_cons(2).all? { |x,y| y == x + 1 } == true ? true : false
+	
+	def free_spaces?(column)
+	  @cage.any? { |row| row[column] == " " }
 	end
-
-	def check_columns
-		c_indices = []
-		r_indices = []
+	
+	def winner
+	  @winner
+	end
+	
+	def check_winner
+	  check_draw
 		@cage.each_with_index do |row, r_index|
-			row.each_with_index do |circle, c_index|
-				if circle != " "
-					c_indices << c_index
-					r_indices << r_index
-				end
-			end
-		end
-		check_for_vertical_duplicates(c_indices, r_indices)
+		  row.each_with_index do |cir, c_index|
+		    if cir == "b" || cir == "r"
+		      
+		      #check horizontal win
+		      if c_index <= 3 && (cir == @cage[r_index][c_index + 1] && cir == @cage[r_index][c_index + 2] && cir == @cage[r_index][c_index + 3])
+		        @winner = true
+		      
+		      #check vertical win
+		      elsif r_index <= 2 && (cir == @cage[r_index + 1][c_index] && cir == @cage[r_index + 2][c_index] && cir == @cage[r_index + 3][c_index])
+		        @winner = true
+		      
+		      #check diagonal win
+		      elsif r_index <= 2 && c_index <= 3 && (cir == @cage[r_index + 1][c_index + 1] && cir == @cage[r_index + 2][c_index + 2] && cir == @cage[r_index + 3][c_index + 3])
+            @winner = true
+          elsif r_index <= 2 && c_index >= 3 && (cir == @cage[r_index + 1][c_index - 1] && cir == @cage[r_index + 2][c_index - 2] && cir == @cage[r_index + 3][c_index - 3])
+            @winner = true
+		      end
+		    end
+		  end
+	  end
 	end
 
-	def check_for_draw
+	def check_draw
 		if @moves >= 42
 		  print_cage
+		  puts "\n"
 		  puts "It's a draw!"
-		  true
-		else
-		  false
-		end
-	end
-
-	def diagonal_matches(positions)
-		if positions.length >= 4
-		  a = positions.map! { |elem| elem.join(', ').to_i }
-		  if a[a.size-1] == a[0] + a.size-1
-		    return true
-		  end
+		  @winner = true
 		end
 	end
 	
-	def check_for_diagonal
-		positions = []
-		@cage.each_with_index do |row, r_index|
-			row.each_with_index do |circle, c_index|
-				if circle != " "
-					positions << [r_index, c_index]
-				end
-			end
-		end
-		if positions != []
-			diagonal_matches(positions)
-		end
+	def declare_winner
+	  print_cage
+	  puts "\n"
+	  puts "Player black wins" if @player_turn == "b"
+	  puts "Player red wins" if @player_turn == "r"
 	end
-
-	def winner
-		if check_rows == true
-		  if @player_turn == "r"
-		    print_cage
-		    puts "Player red wins!"
-		    return true
-		  else
-		  	print_cage
-		    puts "Player black wins!"
-		    return true
-			end
-		elsif check_columns == true
-		  if @player_turn == "r"
-		    print_cage
-  		  puts "Player red wins!"
-  		  return true
-  		else
-  		  print_cage
-  		  puts "Player black wins!"
-  		  return true
-			end
-		elsif check_for_diagonal == true
-			if @player_turn == "r"
-			  print_cage
-  		  puts "Player red wins!"
-  		  return true
-  		else
-  		  print_cage
-  		  puts "Player black wins!"
-  		  return true
-			end
-		else 
-			check_for_draw
-		end
-	end
-
 end
 
 class Player
@@ -192,23 +127,3 @@ class Player
 		@color
 	end
 end
-
-my_game = Cage.new
-player1 = Player.new("b")
-player2 = Player.new("r")
-
-=begin while my_game.winner == false
-  puts "\n"
-  my_game.print_cage
-  print "\n"
-  my_game.prompt_player(player1)
-  if my_game.winner == true
-    break
-  end
-  puts "\n"
-  my_game.print_cage
-  print "\n"
-  my_game.prompt_player(player2)
-  my_game.winner
-end
-=end
